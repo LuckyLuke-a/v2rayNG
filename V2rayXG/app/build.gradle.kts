153ddf489c5942032e91b1938b695b3e254186e0
lucky luke <>
2024-09-20 18:25:14 +0000

    Update to v1.9.6

--- V2rayXG/app/build.gradle.kts
+++ V2rayXG/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.v2ray.xng"
         minSdk = 21
         targetSdk = 34
-        versionCode = 593
-        versionName = "1.9.5"
+        versionCode = 594
+        versionName = "1.9.6"
         multiDexEnabled = true
         splits {
             abi {
